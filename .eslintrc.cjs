5799cc067b55b267e114299c221323b63b016c42
hhui64 <907322015@qq.com>
2023-09-30 05:08:44 +0800

    workflow: 调整选项

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,20 +6,22 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.cjs'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+    'require-atomic-updates': 'off',
+    'no-async-promise-executor': 'off',
+    'no-var-requires': 'off',
   },
-};
+}
